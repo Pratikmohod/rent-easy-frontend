f3a876834451d06a95baa67caeb4b83982c55a30
Pratik Mohod <pratikmohod0311@gmail.com>
2026-08-13 01:25:35 +0530

    change addProperty navigation from myProfile to properties

--- src/pages/landlord/AddProperty.jsx
+++ src/pages/landlord/AddProperty.jsx
@@ -105,7 +105,7 @@ const AddProperty = () => {
   useEffect(() => {
     if (responseCode === 201) {
       dispatch(clearResponseCode());
-      navigate("/homePage");
+      navigate("/properties");
     }
   }, [responseCode, dispatch, navigate]);
 
